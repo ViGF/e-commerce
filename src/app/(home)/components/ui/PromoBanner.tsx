1f42e9148ae9b036bbac06d9f1890e2cb9ec2c3f
ViGF <vitoriogdef@gmail.com>
2023-10-27 16:50:33 -0300

    feat: add PromoBanner and SectionTitle components

---
+++ src/app/(home)/components/ui/PromoBanner.tsx
@@ -0,0 +1,14 @@
+import Image, { ImageProps } from "next/image";
+
+export function PromoBanner({ alt, ...props }: ImageProps) {
+  return (
+    <Image
+      height={0}
+      width={0}
+      className="h-auto w-full px-5 mt-8"
+      sizes="100vw"
+      alt={alt}
+      {...props}
+    />
+  );
+}
